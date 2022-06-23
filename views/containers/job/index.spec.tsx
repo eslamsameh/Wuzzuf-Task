a6fd1d9ec0ab1c0efb2c11b2a31b596ad716e64e
eslam.abdelmoniem <eslam.abdelmoniem@vodafone.com>
2022-06-23 16:30:14 +0200

    add job UT

--- views/containers/job/index.spec.tsx
+++ views/containers/job/index.spec.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { SingleJob } from '.';
 import * as redux from 'react-redux';
+import { jobMockedData } from 'mocks';
 
 const mockDispatch = jest.fn();
 
@@ -10,12 +11,34 @@ jest.mock('react-redux', () => ({
   useDispatch: () => jest.fn(),
 }));
 
-let selectorMock = { job: { job: 'test' }, jobStatus: {} };
+let selectorMock: any = { job: { job: { job: {} }, jobStatus: { loading: true } } };
 
 describe('Test Job Page', () => {
-  it('should Card component to be fired with Header string and Footer string', () => {
+  beforeEach(() => {
+    jest.spyOn(React, 'useEffect').mockImplementationOnce((cb: any) => cb()());
+  });
+  it('should render Job Page with an empty Object and loading is true', () => {
     jest.spyOn(redux, 'useSelector').mockImplementation((callback) => callback(selectorMock));
     const wrapper = shallow(<SingleJob></SingleJob>);
+    expect(selectorMock.job.job.id).toBeUndefined();
+    expect(selectorMock.job.jobStatus.loading).toBe(true);
+    expect(wrapper).toBeDefined();
+  });
+
+  it('should render Job Page with job Object', () => {
+    selectorMock = { job: { job: { job: jobMockedData }, jobStatus: { loading: false } } };
+    jest.spyOn(redux, 'useSelector').mockImplementation((callback) => callback(selectorMock));
+    const wrapper = shallow(<SingleJob></SingleJob>);
+    expect(selectorMock.job.job).toBeDefined();
+    expect(selectorMock.job.jobStatus.loading).toBe(false);
+    expect(wrapper).toBeDefined();
+  });
+
+  it('should render Job Page with an undefined job value', () => {
+    selectorMock = { job: undefined };
+    jest.spyOn(redux, 'useSelector').mockImplementation((callback) => callback(selectorMock));
+    const wrapper = shallow(<SingleJob></SingleJob>);
+    expect(selectorMock.job).toBeUndefined();
     expect(wrapper).toBeDefined();
   });
 });
